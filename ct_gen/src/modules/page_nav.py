5892924e818100a064cd9bb56a8afbfdef36be99
unknown <natashanewboldlondon@gmail.com>
2023-10-22 21:26:17 +0100

    Write to google, Moderate page 3 and 4, add feedback feature.

--- ct_gen/src/modules/page_nav.py
+++ ct_gen/src/modules/page_nav.py
@@ -4,6 +4,7 @@ def forward_button(col, button_text):
     if col.button(button_text):
         st.session_state["page_number"] = st.session_state["page_number"] + 1
         st.experimental_rerun()
+        
 def backward_button(col, button_text):
     if col.button(button_text):
         st.session_state["page_number"] = st.session_state["page_number"] - 1
