6396e84b809883d99efa8ed5ff8f49a26eed9abe
datamaunz <datamaunz@gmail.com>
2023-12-24 09:02:21 -0700

    :fire: cleaning up session state definitions

--- ct_gen/src/modules/initialize_session_state.py
+++ ct_gen/src/modules/initialize_session_state.py
@@ -1,38 +1,19 @@
 import streamlit as st
 import openai
 
-
 def initalize_session_state_dict():
     # Initialize OpenAI API connection
     openai.api_key = st.secrets["openai"]["api_key"]
     
-    st.session_state["model_name"] = "gpt-3.5-turbo"
+    # functional 
+    st.session_state["model_name"] = "gpt-4"
     
-    if "prompt" not in st.session_state:
-        st.session_state["prompt"] = ""
-    
+    if "change_tracker" not in st.session_state:
+        st.session_state["change_tracker"] = 0
     if "page_number" not in st.session_state:
         st.session_state["page_number"] = 1
         
-    # Initialize the user inputs and generated conspiracy theory in session state
-    if 'url' not in st.session_state:
-        st.session_state.url = ""
-    if 'selected_version' not in st.session_state:
-        st.session_state["selected_version"] = ""
-    if 'selected_article_content' not in st.session_state:
-        st.session_state["selected_article_content"] = ""
-    if 'culprits' not in st.session_state:
-        st.session_state.culprits = ""
-    if 'goals' not in st.session_state:
-        st.session_state.goals = ""
-    if 'motive_info' not in st.session_state:
-        st.session_state.motive_info = ""  
-    if 'conspiracy_theory' not in st.session_state:
-        st.session_state.conspiracy_theory = ""
-    if "change_tracker" not in st.session_state:
-        st.session_state["change_tracker"] = 0
-        
-    
+    # news
     if "news_name" not in st.session_state:
         st.session_state["news_name"] = ""
     if "news_summary" not in st.session_state:
@@ -40,17 +21,24 @@ def initalize_session_state_dict():
     if "news_img" not in st.session_state:
         st.session_state["news_img"] = ""
     
+    # culprits
     if "culprits_name" not in st.session_state:
         st.session_state["culprits_name"] = ""
     if "culprits_summary" not in st.session_state:
         st.session_state["culprits_summary"] = ""
     if "culprits_img" not in st.session_state:
         st.session_state["culprits_img"] = ""
-        
+    
+    # motives
     if "motives_name" not in st.session_state:
         st.session_state["motives_name"] = ""
     if "motives_summary" not in st.session_state:
         st.session_state["motives_summary"] = ""
     if "motives_img" not in st.session_state:
         st.session_state["motives_img"] = ""
-        
+    
+    # output
+    if "prompt" not in st.session_state:
+        st.session_state["prompt"] = ""
+    if 'conspiracy_theory' not in st.session_state:
+        st.session_state.conspiracy_theory = ""
