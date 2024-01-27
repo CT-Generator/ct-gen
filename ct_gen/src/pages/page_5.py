de1ac9e9a60c5465d3365f1a45f392d4742f2d2d
thrasher995 <mohammed.darras@outlook.com>
2024-01-27 15:39:15 +0300

    reverting changes in requirements.txt + updated page_5

--- ct_gen/src/pages/page_5.py
+++ ct_gen/src/pages/page_5.py
@@ -8,8 +8,8 @@ from ct_gen.src.modules.image_functions import *
 def display_page_5():
     
     image_path = "ct_gen/data/images/motives"
-    step_title = "Step 4"
-    title = "The Motives"
+    step_title = "Step 3"
+    title = "The Motive"
     info = "What's their endgame? Every conspiracy theory has a motive. Select one of the options below."
     sheet_name = "motives"
     instruction = "Select a motive"
