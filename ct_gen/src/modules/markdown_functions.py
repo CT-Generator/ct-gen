fd72b909e1a6d04bcc94c9c63ed3154d285d7c81
thrasher995 <mohammed.darras@outlook.com>
2024-04-12 08:20:53 +0300

    added .css file to customize the generated image

--- ct_gen/src/modules/markdown_functions.py
+++ ct_gen/src/modules/markdown_functions.py
@@ -5,9 +5,8 @@ from io import BytesIO
 def markdown_to_image(markdown_text):
     # Convert Markdown to HTML
     html_content = markdown2.markdown(markdown_text)
-
+    
     # Convert HTML to an image
     imgkit_options = {'format': 'png'}
-    img_bytes = imgkit.from_string(html_content, False, options=imgkit_options)
-
+    img_bytes = imgkit.from_string(html_content,False, options= imgkit_options, css= "ct_gen\\data\\css\\CT-img.css")
     return img_bytes
