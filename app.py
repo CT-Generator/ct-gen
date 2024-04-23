3566bb4465f2d4c8425c0ecbe8d4d666e3f2aac2
Mohammed Darras <98810891+thrasher995@users.noreply.github.com>
2024-04-23 12:00:29 +0300

    Update app.py
    Removed import of check_password()

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from ct_gen.src.pages.page_3 import display_page_3
 from ct_gen.src.pages.page_4 import display_page_4
 from ct_gen.src.pages.page_5 import display_page_5
 from ct_gen.src.modules.initialize_session_state import initalize_session_state_dict
-from ct_gen.src.modules.authentication import check_password, load_secrets
+from ct_gen.src.modules.authentication import load_secrets
 
 
 
